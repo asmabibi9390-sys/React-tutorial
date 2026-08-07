4702e50ad4d2fc1e61de12585262c174bd9f1ad3
asmabibi9390-sys <asmabibi9390@gmail.com>
2026-08-07 19:57:45 +0500

    complete project 5 counter.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
 import "./App.css";
-import StudentCard from "./components/StudentCard";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
-      <h1 className="heading">React Props Example</h1>
-
-      <StudentCard
-        name="Asma Khan"
-        age={20}
-        course="React.js"
-      />
-
-      <StudentCard
-        name="Ali Khan"
-        age={22}
-        course="JavaScript"
-      />
-
-      <StudentCard
-        name="Ayesha"
-        age={19}
-        course="HTML & CSS"
-      />
+      <h1>React Counter App</h1>
+      <Counter />
     </>
   );
 }
