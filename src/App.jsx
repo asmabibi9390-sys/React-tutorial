bb738ea5b6bcac13d5cd255c293aaca2c98784bb
asmabibi9390-sys <asmabibi9390@gmail.com>
2026-08-07 19:35:50 +0500

    project 3- react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 import "./App.css";
-import hero from "./assets/hero.png";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
 
-  const name = "Asma";
-  const course = "React.js";
-  const age = 20;
-
   return (
     <>
-      <h1 className="heading"> Welcome to My React Journey</h1>
-
-      <img src={hero} alt="Hero" className="hero-img" />
-
-      <h2>Hello, {name} </h2>
-
-      <h3>Course: {course}</h3>
-
-      <h3>Age: {age}</h3>
-
-      <p>
-        Learning React.js one component at a time and building beautiful
-        websites.
-      </p>
-
-      <h3>10 + 20 = {10 + 20}</h3>
-
-      <button className="btn">Explore More</button>
+      <Navbar />
+      <Hero />
+      <Footer />
     </>
   );
+
 }
 
 export default App;
